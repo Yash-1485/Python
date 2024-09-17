7ed0d42dc2c3ec64ae27fb5498fdb89352e13bf4
Yash1485 <yashparekh140805@gmail.com>
2024-09-17 08:41:49 +0530

    Added Pattern file

---
+++ Patterns.py
@@ -0,0 +1,256 @@
+n=5
+
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print('*',end=' ')
+#     print()
+
+#ok
+
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if (i==1 or j==1 or i==n or j==n or (i+j)%2==0): print('*',end='')
+#         else: print(end=' ')
+#     print()
+
+# for i in range(1,n+1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,n+1):
+#         print('*',end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,n+1):
+#         if (i==1 or j==1 or i==n or j==n): print('*',end='')
+#         else: print(end=' ')
+#     print()
+
+# for i in range(1,n+1):
+#     for k in range(1,i):
+#         print(end=' ')
+#     for j in range(1,n+1):
+#         print('*',end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for k in range(1,i):
+#         print(end=' ')
+#     for j in range(1,n+1):
+#         if (i==1 or j==1 or i==n or j==n): print('*',end='')
+#         else: print(end=' ')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print('*',end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         if (i==1 or j==1 or i==n or j==i):print('*',end='')
+#         else:print(end=' ')
+#     print()
+
+# for i in range(1,n+1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,i+1):
+#         print('*',end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,i+1):
+#         if (i==1 or j==1 or i==n or j==i):print('*',end='')
+#         else:print(end=' ')
+#     print()
+    
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print('*',end='')
+#     print()
+
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         if (i==1 or j==1 or i==n or j==i):print('*',end='')
+#         else:print(end=' ')
+#     print()
+
+# for i in range(n,0,-1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,i+1):
+#         print('*',end='')
+#     print()
+
+# for i in range(n,0,-1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,i+1):
+#         if(i==1 or j==1 or i==n or j==i):print('*',end='')
+#         else:print(end=' ')
+#     print()
+
+# for i in range(1,n+1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,i+1):
+#         print('*',end=' ')
+#     print()
+
+# for i in range(1,n+1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,i+1):
+#         if(i==1 or j==1 or i==n or j==i):print('*',end=' ')
+#         else:print(end='  ')
+#     print()
+
+# for i in range(n,0,-1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,i+1):
+#         print('*',end=' ')
+#     print()
+
+# for i in range(n,0,-1):
+#     for k in range(n,i,-1):
+#         print(end=' ')
+#     for j in range(1,i+1):
+#         if (i==1 or j==1 or i==n or j==i):print('*',end=' ')
+#         else:print(end='  ')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end='')
+#     print()
+
+# for i in range(n,0,-1):
+#     for j in range(n,i-1,-1):
+#         print(i,end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(i,0,-1):
+#         print(j,end='')
+#     print()
+
+# for i in range(n,0,-1):
+#     for j in range(n,i-1,-1):
+#         print(j,end='')
+#     print()
+
+# for i in range(n,0,-1):
+#     for j in range(i,n+1):
+#         print(j,end='')
+#     print()
+
+# for i in range(1,n+1):        
+#     k=i
+#     for j in range(1,i+1):
+#         print(k,end='')
+#         k+=1
+#     print()
+
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(i,end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(n,i-1,-1):
+#         print(i,end='')
+#     print()
+
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end='')
+#     print()
+
+# for i in range(n,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(n,i-1,-1):
+#         print(j,end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(i,n+1):
+#         print(j,end='')
+#     print()
+
+# for i in range(n,0,-1):
+#     k=i
+#     for j in range(1,i+1):
+#         print(k,end='')
+#         k+=1
+#     print()
+
+# n*=2
+# for i in range(1,n+1,2):
+#     for j in range(i,n+1,2):
+#         print(j,end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         if (j%2==0):print(0,end='')
+#         else:print(1,end='')
+#     print()
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         if (i%2==0):print(0,end='')
+#         else:print(1,end='')
+#     print()
+
+# count=1
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(count%2,end='')
+#         count+=1
+#     print()
+
+#Method1
+# count=1
+# for i in range(1,n+1):    
+#     if i%2==0:
+#         k=count-1+i
+#     for j in range(1,i+1):    
+#         if i%2==0:
+#             print(k,end='')
+#             k-=1
+#             count+=1
+#         else:
+#             print(count,end='')
+#             count+=1
+#     print()
+
+#Method2
+# count=0
+# for i in range(1,n+1):    
+#     if i%2==0:
+#         count=count-1+i
+#     else:
+#         count=count+i
+#     for j in range(1,i+1):    
+#         if i%2==0:
+#             print(count,end='')
+#             count-=1
+#         else:
+#             print(count,end='')
+#             count+=1
+#     print()
