88dbec58d0271ec797e8c8ff44494c32ed13766b
Yash1485 <yashparekh140805@gmail.com>
2024-09-27 10:27:44 +0530

    Added Calculator.py module in Modules folder

---
+++ test.py
@@ -0,0 +1,7 @@
+import Modules.Calculator as calc
+# print(calc.addition(1,2,3,4,5,6,7,8,9,10,-10,-9,-8,-7,-6,-5,-4,-3,-2))
+# print(calc.multiplication(2,5,1,6,8,2,4))
+# print(calc.subtraction(1,2,3,4,5,1,-1,-2,-3,-4,-5))
+# print(calc.division(4,3))
+# print(calc.modulation(10,4))
+# print(calc.power(100,0))
